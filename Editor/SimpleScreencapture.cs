f5578d4d468b4b80d6f94fd730330c87c018e3ef
bamjadu <bamjadu@naver.com>
2020-09-10 10:08:53 +0900

    [TOOL] Upload Initial files

---
+++ Editor/SimpleScreencapture.cs
@@ -0,0 +1,314 @@
+﻿
+
+
+
+#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+using System.IO;
+
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+using UnityEditor.EditorTools;
+
+namespace TemplateTools
+{
+    public class SimpleScreencapture
+    {
+
+        static float captureSizeMultiplier = 1.0f;
+
+        //static LocalizationHelper currentHelper;
+
+
+        static string[] renderResolution =
+        {
+            "720p - 1280x720",
+            "1080p - 1920x1080",
+            "4K - 3840x2160",
+            "8K - 7680x4320",
+            "Custom - Screen"
+        };
+
+        public static int selectedResolution = 1;
+
+
+        public static string[] GetRenderResolutions()
+        {
+            return renderResolution;
+        }
+
+
+        /*
+        [InitializeOnLoadMethod]
+        public static void InitLocalizationModule()
+        {
+            currentHelper = new LocalizationHelper(typeof(SimpleScreencapture).ToString());
+            currentHelper.RefreshDictionary();
+        }
+        */
+
+
+        static void DoCaptureOnCamera(Camera cam)
+        {
+            if (cam == null)
+                return;
+
+            Debug.Log("Scene cameras : " + cam.name);
+
+            string currentScreenshot = Path.Combine(Path.GetTempPath(), "Capture_" + SceneManager.GetActiveScene().name + "_" + cam.name.Replace(" ", "_") + "_" + System.DateTime.Now.ToLongTimeString().Replace(" ", "_").Replace(":", "_") + ".png");
+
+            Debug.Log("Capture file : " + currentScreenshot);
+
+            OnCaptureScreenshot(cam, captureSizeMultiplier, cam.pixelWidth, cam.pixelHeight, currentScreenshot);
+
+            
+        }
+
+
+        public static void DoCaptureOnCurrentSceneBySize(int selectedResIndex, float sizeMultiplier)
+        {
+            captureSizeMultiplier = sizeMultiplier;
+            DoCaptureOnCurrentScene(selectedResIndex);
+        }
+
+        public static void DoCaptureOnActiveCameras(float sizeMultiplier)
+        {
+            captureSizeMultiplier = sizeMultiplier;
+            DoCaptureOnActiveCameras();
+        }
+
+
+        static Vector2Int GetCaptureResolution(int selectedResIndex)
+        {
+            Vector2Int outVector = Vector2Int.one;
+
+            if (selectedResIndex == renderResolution.Length - 1)
+                return outVector;
+
+            string[] tokens = renderResolution[selectedResIndex].Split(' ');
+
+            string temp = tokens[tokens.Length - 1];
+            tokens.Initialize();
+            tokens = temp.Split('x');
+
+            outVector = new Vector2Int(int.Parse(tokens[0]), int.Parse(tokens[1]));
+
+            return outVector;
+        }
+
+
+        public static void DoCaptureOnSelection(Rect rect)
+        {
+            Camera currentSceneCamera = SceneView.GetAllSceneCameras()[0];
+
+            Debug.Log("Scene cameras : " + SceneView.GetAllSceneCameras().Length.ToString());
+
+            string currentScreenshot = Path.Combine(Path.GetTempPath(), "Capture_" + SceneManager.GetActiveScene().name + "_" + System.DateTime.Now.ToLongTimeString().Replace(" ", "_").Replace(":", "_") + ".png");
+
+            Debug.Log("Capture file : " + currentScreenshot);
+
+            Vector2 pos  = HandleUtility.GUIPointToScreenPixelCoordinate(new Vector2(rect.x, rect.y));
+            Vector2 pos1 = HandleUtility.GUIPointToScreenPixelCoordinate(new Vector2(rect.x + rect.width, rect.y + rect.height));
+
+            rect.x = pos.x;
+            rect.y = currentSceneCamera.pixelHeight - pos.y;
+            rect.width = Mathf.Abs(pos1.x - pos.x);
+            rect.height = Mathf.Abs(pos1.y - pos.y);
+
+            OnCaptureScreenshot(currentSceneCamera, rect, currentScreenshot);
+        }
+
+        
+        [MenuItem("Template/Capture Screenshot/Run \"Screen Capturer\"", priority = 10)]
+        
+        public static void ActivateScreencaptureTool()
+        {
+            EditorTools.SetActiveTool<SimpleScreencaptureTool>();
+        }
+
+
+        [MenuItem("Template/Capture Screenshot/Capture current scene &_p", priority = 30)]
+        public static void DoCaptureOnCurrentScene()
+        {
+            DoCaptureOnCurrentScene(selectedResolution);
+        }
+
+
+        //[MenuItem("Template/Capture Screenshot/Capture current scene &_p", priority = 20)]
+        public static void DoCaptureOnCurrentScene(int selectedResIndex)
+        {
+            Camera currentSceneCamera = SceneView.GetAllSceneCameras()[0];
+
+            Debug.Log("Scene cameras : " + SceneView.GetAllSceneCameras().Length.ToString());
+
+            string currentScreenshot = Path.Combine(Path.GetTempPath(), "Capture_" + SceneManager.GetActiveScene().name + "_" + System.DateTime.Now.ToLongTimeString().Replace(" ", "_").Replace(":", "_") + ".png");
+
+            Debug.Log("Capture file : " + currentScreenshot);
+
+            Vector2Int captureRes = GetCaptureResolution(selectedResIndex);
+
+            if (selectedResIndex == renderResolution.Length - 1)
+                OnCaptureScreenshot(currentSceneCamera, captureSizeMultiplier, currentSceneCamera.pixelWidth, currentSceneCamera.pixelHeight, currentScreenshot);
+            else
+                OnCaptureScreenshot(currentSceneCamera, 1.0f, captureRes.x, captureRes.y, currentScreenshot);
+        }
+
+        [MenuItem("Template/Capture Screenshot/Capture current scene &_p", validate = true)]
+        static bool IsEnabledCapture()
+        {
+            if (SceneView.GetAllSceneCameras().Length == 0)
+                return false;
+
+            if (SceneManager.GetActiveScene() == null)
+                return false;
+
+            return true;
+        }
+
+
+        [MenuItem("Template/Capture Screenshot/Capture by active cameras", priority = 40)]
+        public static void DoCaptureOnActiveCameras()
+        {
+            Camera[] cameras = Component.FindObjectsOfType<Camera>();
+
+            foreach (Camera currentCam in cameras)
+            {
+                if (currentCam.isActiveAndEnabled)
+                    DoCaptureOnCamera(currentCam);
+            }
+
+        }
+
+
+        [MenuItem("Template/Capture Screenshot/Explore screenshot depot &_o", priority = 70)]
+        public static void DoExploreScreenshotDepot()
+        {
+            Application.OpenURL(Path.GetTempPath());
+        }
+
+        [MenuItem("Template/Capture Screenshot/Explore screenshot depot &_o", validate = true)]
+        static bool IsThereScreenshotFilesAtDepot()
+        {
+            if (Directory.GetFiles(Path.GetTempPath(), "Capture_*.png").Length == 0)
+                return false;
+             
+            return true;
+        }
+
+
+        static void OnCaptureScreenshot(Camera camera, float sizeMultiplier, int width, int height, string savepath)
+        {
+
+            Debug.Log("SceneView name : " + SceneManager.GetActiveScene().name);
+            Debug.Log("Allowed dynamic resolution ? : " + camera.allowDynamicResolution.ToString());
+            Debug.Log("Camera Pixel width : " + camera.pixelWidth.ToString());
+            Debug.Log("Camera Pixel height : " + camera.pixelHeight.ToString());
+
+
+
+            int renderWidth;
+            int renderHeight;
+
+            //renderWidth = Mathf.RoundToInt(camera.pixelWidth * sizeMultiplier);
+            //renderHeight = Mathf.RoundToInt(camera.pixelHeight * sizeMultiplier);
+            renderWidth = Mathf.RoundToInt(width * sizeMultiplier);
+            renderHeight = Mathf.RoundToInt(height * sizeMultiplier);
+
+
+            
+
+            RenderTexture renderTexture = new RenderTexture(renderWidth, renderHeight, 16, RenderTextureFormat.ARGB32);
+
+            float originalAspect = camera.aspect;
+            bool originalMSAA = camera.allowMSAA;
+
+            camera.aspect = (float)renderWidth / (float)renderHeight;
+            camera.allowMSAA = true;
+
+
+            camera.targetTexture = renderTexture;
+
+
+            Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+
+
+            camera.Render();
+            RenderTexture.active = renderTexture;
+
+
+            Rect rect = new Rect(0, 0, renderTexture.width, renderTexture.height);
+            renderResult.ReadPixels(rect, 0, 0);
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(savepath, byteArray);
+
+            camera.targetTexture = null;
+
+
+            camera.aspect = originalAspect;
+            camera.allowMSAA = originalMSAA;
+
+
+
+            RenderTexture.active = null;
+
+            camera.ResetAspect();
+
+
+            SceneView.lastActiveSceneView.ShowNotification(new GUIContent("Screen Captured."), 4);
+
+        }
+
+
+        static void OnCaptureScreenshot(Camera camera, Rect rect, string savepath)
+        {
+            float sizeMultiplier = 1.0f;
+
+            int renderWidth = camera.pixelWidth;
+            int renderHeight = camera.pixelHeight;
+
+
+            RenderTexture renderTexture = new RenderTexture(renderWidth, renderHeight, 16, RenderTextureFormat.ARGB32);
+
+            float originalAspect = camera.aspect;
+            camera.aspect = (float)renderWidth / (float)renderHeight;
+
+
+            camera.targetTexture = renderTexture;
+
+
+            //Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Texture2D renderResult = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.ARGB32, false);
+
+
+            camera.Render();
+            RenderTexture.active = renderTexture;
+
+            Debug.Log(rect);
+            renderResult.ReadPixels(rect, 0, 0, false);
+            
+
+            byte[] byteArray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes(savepath, byteArray);
+
+            camera.targetTexture = null;
+            camera.aspect = originalAspect;
+            RenderTexture.active = null;
+
+            camera.ResetAspect();
+
+
+            SceneView.lastActiveSceneView.ShowNotification(new GUIContent("Screen Captured."), 4);
+
+
+        }
+    }
+
+}
+#endif
---
+++ Editor/SimpleScreencaptureTool.cs
@@ -0,0 +1,278 @@
+﻿
+#if UNITY_EDITOR
+
+using System;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.EditorTools;
+
+
+namespace TemplateTools
+{
+
+    enum CaptureStatus
+    {
+        Idle,
+        PrepareForCaptureArea,
+        CapturingOnArea
+    }
+
+
+    [EditorTool("Simple Screen Capture Tool")]
+    class SimpleScreencaptureTool : EditorTool
+    {
+        
+        [SerializeField]
+        public Texture2D m_ToolIcon;
+
+        [SerializeField]
+        public Texture2D captureIcon;
+
+        [SerializeField]
+        public Texture2D multiCaptureIcon;
+
+        [SerializeField]
+        public Texture2D selCaptureIcon;
+
+        [SerializeField]
+        public Texture2D exploreIcon;
+
+        //[SerializeField]
+        //public Texture2D settingIcon;
+
+        GUIContent m_IconContent;
+
+        GUIContent captureIconContent;
+        GUIContent exploreIconContent;
+        GUIContent multiCaptureIconContent;
+        GUIContent settingIconContent;
+
+        static float hSliderValue;
+
+        bool status = false;
+
+        bool cap = false;
+
+        Rect mouseRect = new Rect(0, 0, 0, 0);
+
+        CaptureStatus currentCaptureStatus = CaptureStatus.Idle;
+
+        void OnEnable()
+        {
+            m_IconContent = new GUIContent()
+            {
+                image = m_ToolIcon,
+                text = "Simple Screen Capture Tool",
+                tooltip = "Screenshot"
+            };
+
+            captureIconContent = new GUIContent()
+            {
+                image = captureIcon,
+                tooltip = "Capture Single"
+
+            };
+
+            exploreIconContent = new GUIContent()
+            {
+                image = exploreIcon,
+                tooltip = "Explore the screenshot repository"
+            };
+
+            multiCaptureIconContent = new GUIContent()
+            {
+                image = multiCaptureIcon,
+                tooltip = "Capture Multiple"
+            };
+
+            /*
+            settingIconContent = new GUIContent()
+            {
+                image = settingIcon,
+                tooltip = "Setting"
+            };
+            */
+
+            hSliderValue = 1.0f;
+        }
+
+        public override GUIContent toolbarIcon
+        {
+            get { return m_IconContent; }
+        }
+
+       
+        
+        
+        public override void OnToolGUI(EditorWindow window)
+        {
+
+            //window.wantsMouseMove = true;
+            
+
+            Event current = Event.current;
+
+
+            if (current.type == EventType.Layout)
+            {
+                HandleUtility.AddDefaultControl(0);
+            }
+
+
+            if (current.type == EventType.MouseDown && currentCaptureStatus != CaptureStatus.Idle)
+            {
+                Vector2 p = HandleUtility.GUIPointToScreenPixelCoordinate(current.mousePosition);
+
+                //Debug.Log(Screen.height);
+                //Debug.Log(Camera.current.pixelHeight);
+                
+                mouseRect.x = current.mousePosition.x;
+                mouseRect.y = current.mousePosition.y;
+                
+                //mouseRect.x = p.x;
+                //mouseRect.y = Camera.current.pixelHeight - p.y;
+
+                current.Use();
+
+                Debug.Log(mouseRect);
+                //Debug.Log(Camera.current.pixelWidth);
+                //Debug.Log(Camera.current.scaledPixelWidth);
+
+                //Debug.Log(GUIUtility.GUIToScreenPoint(current.mousePosition));
+
+                //Debug.Log("Wat : " + HandleUtility.GUIPointToScreenPixelCoordinate(current.mousePosition));
+
+
+
+                //Debug.Log(GUIUtility.GUIToScreenPoint(current.mousePosition) - new Vector2(SceneView.currentDrawingSceneView.position.x, SceneView.currentDrawingSceneView.position.y));
+
+                //Debug.Log("SceneView Position : " );
+
+                //Debug.Log("Window Pos : " + window.position);
+                //Debug.Log("Window Max Size : " + window.maxSize);
+
+
+
+            }
+
+            if (current.type == EventType.MouseDrag && currentCaptureStatus != CaptureStatus.Idle)
+            {
+
+                Vector2 p = HandleUtility.GUIPointToScreenPixelCoordinate(current.mousePosition);
+
+
+                mouseRect.width = Mathf.Abs(current.mousePosition.x - mouseRect.x);
+                mouseRect.height = Mathf.Abs(current.mousePosition.y - mouseRect.y);
+
+                currentCaptureStatus = CaptureStatus.CapturingOnArea;
+
+                current.Use();
+            }
+
+            if (current.type == EventType.MouseUp && currentCaptureStatus != CaptureStatus.Idle)
+            {
+                current.Use();
+
+                
+                currentCaptureStatus = CaptureStatus.Idle;
+
+                SimpleScreencapture.DoCaptureOnSelection(mouseRect);
+                Debug.Log(mouseRect + " : Captured.");
+                mouseRect = Rect.zero;
+            }
+
+            
+            if (current.type == EventType.MouseLeaveWindow && currentCaptureStatus == CaptureStatus.CapturingOnArea)
+            {
+                //Debug.Log("out!");
+                current.Use();
+
+                SimpleScreencapture.DoCaptureOnSelection(mouseRect);
+                Debug.Log(mouseRect + " : Captured.");
+
+                mouseRect = Rect.zero;
+                currentCaptureStatus = CaptureStatus.Idle;
+            }
+            
+
+            Handles.BeginGUI();
+
+            if (currentCaptureStatus != CaptureStatus.Idle)
+                Handles.DrawSolidRectangleWithOutline(mouseRect, new Color(0.5f, 0.5f, 0.5f, 0.25f), Color.yellow);
+
+
+            GUILayout.Window(1234, new Rect(window.position.width - 170, window.position.height - 125, 160, 115), (id) =>
+            {
+
+                GUILayout.BeginVertical("Box");
+
+                status = EditorGUILayout.BeginFoldoutHeaderGroup(status, new GUIContent("Resolution"));
+
+                if (status)
+                {
+                    SimpleScreencapture.selectedResolution = EditorGUILayout.Popup(SimpleScreencapture.selectedResolution, SimpleScreencapture.GetRenderResolutions());
+
+                    if (SimpleScreencapture.selectedResolution == 4)
+                        hSliderValue = EditorGUILayout.Slider(hSliderValue, 0.05f, 8f);
+                }
+
+                EditorGUILayout.EndFoldoutHeaderGroup();
+
+                GUILayout.EndVertical();
+
+                GUILayout.BeginHorizontal("Box");
+
+                GUIStyle style = new GUIStyle(GUI.skin.button);
+
+                if (GUILayout.Button(captureIconContent, style, GUILayout.Width(32), GUILayout.Height(32)))
+                {
+                    currentCaptureStatus = CaptureStatus.Idle; 
+                    SimpleScreencapture.DoCaptureOnCurrentSceneBySize(SimpleScreencapture.selectedResolution, hSliderValue); 
+                }
+
+                if (GUILayout.Button(new GUIContent(selCaptureIcon, "Capture Selected"), GUILayout.Width(32), GUILayout.Height(32)))
+                {
+                    currentCaptureStatus = CaptureStatus.PrepareForCaptureArea;
+                    SceneView.lastActiveSceneView.ShowNotification(new GUIContent("Select an area for capturing."), 3);
+                }
+
+                if (GUILayout.Button(multiCaptureIconContent, style, GUILayout.Width(32), GUILayout.Height(32)))
+                {
+                    currentCaptureStatus = CaptureStatus.Idle;
+                    SimpleScreencapture.DoCaptureOnActiveCameras(hSliderValue);
+                }
+
+                if (GUILayout.Button(exploreIconContent, style, GUILayout.Width(32), GUILayout.Height(32)))
+                {
+                    currentCaptureStatus = CaptureStatus.Idle;
+                    SimpleScreencapture.DoExploreScreenshotDepot();
+                }
+
+                /*
+                if (GUILayout.Button(settingIconContent, style, GUILayout.Width(32), GUILayout.Height(32)))
+                {
+                    currentCaptureStatus = CaptureStatus.Idle;
+                }
+                */
+
+                GUILayout.EndHorizontal();
+
+                //GUILayout.BeginHorizontal("Box");
+
+                
+                
+                //GUILayout.EndHorizontal();
+
+
+                GUI.DragWindow();
+
+            }, new GUIContent("Screen Capture"), GUILayout.Width(150));
+
+            Handles.EndGUI();
+
+            
+        }
+    }
+}
+
+#endif
